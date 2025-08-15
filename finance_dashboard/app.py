4db748f1013267f3195d1928ab87c95df56004ed
Rezi Sabashvili <rezi.sabashvili@etudiant.univ-reims.fr>
2025-08-15 11:49:50 +0200

    final

--- finance_dashboard/app.py
+++ finance_dashboard/app.py
@@ -1,4 +1,3 @@
-# app.py - Banking Market Intelligence Dashboard (final)
 import streamlit as st
 import yfinance as yf
 import pandas as pd
@@ -12,29 +11,8 @@ import tempfile
 import os
 from datetime import datetime
 import matplotlib.pyplot as plt
-import streamlit.components.v1 as components
-
-index_path = os.path.join(os.path.dirname(__file__), "static", "index.html")
-
-GA_JS = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VLN2SS4TBC"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-VLN2SS4TBC');
-</script>
-"""
-
-# Injecte le script GA dans le head via un composant HTML
-components.html(f"<head>{GA_JS}</head>", height=0)
-
-with open(index_path) as f:
-    st.markdown(f.read(), unsafe_allow_html=True)
 
 st.cache_data.clear()
-
 
 # --- PAGE CONFIG ---
 st.set_page_config(
